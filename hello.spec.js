8fc95f3a5c61af2418590946fc760b7205682111
e <1456958517@qq.com>
2017-05-18 20:55:22 +0800

    空

--- hello.spec.js
+++
@@ -1,9 +0,0 @@
-describe('hello, unit test.', function(){
-  it('should also be able to test', function(){
-    expect(true).toBe(true);
-  });
-  
-  it('should be failed', function(){
-      expect(true).toBe(false);
-  })
-});
